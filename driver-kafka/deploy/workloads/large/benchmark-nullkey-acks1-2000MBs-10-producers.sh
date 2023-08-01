397444a12d937738e61705d37d7777ddde6ac2f2
nhaq-confluent <nhaq@confluent.io>
2023-08-01 15:11:16 -0400

    added KRaft implementation

--- driver-kafka/deploy/workloads/large/benchmark-nullkey-acks1-2000MBs-10-producers.sh
+++ driver-kafka/deploy/workloads/large/benchmark-nullkey-acks1-2000MBs-10-producers.sh
@@ -5,17 +5,17 @@ echo "Writing workload files"
 cat > workloads/benchmark-nullkey-acks1-2000MBs-10-producers-step1.yaml << EOF
 name: benchmark-nullkey-acks1-2000MBs-10-producers-step1__AK-2000MBs
 
-topics: 1
+topics: 3
 partitionsPerTopic: 288
 messageSize: 1024
 payloadFile: "payload/payload-1Kb.data"
 subscriptionsPerTopic: 1
-consumerPerSubscription: 10
-producersPerTopic: 10
-producerRate: 2000000
+consumerPerSubscription: 5
+producersPerTopic: 5
+producerRate: 1024000
 consumerBacklogSizeGB: 0
-testDurationMinutes: 60
-warmupDurationMinutes: 10
+testDurationMinutes: 10
+warmupDurationMinutes: 2
 EOF
 
 echo "Starting benchmark"
