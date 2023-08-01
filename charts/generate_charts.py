0473bdf846e32c006719ce930b20557d10287396
nhaq-confluent <nhaq@confluent.io>
2023-08-01 15:10:45 -0400

    added KRaft implementation

--- charts/generate_charts.py
+++ charts/generate_charts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/opt/homebrew/bin/python3.11
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
